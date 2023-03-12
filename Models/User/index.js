cbc7e98877e8f6d641c40df72941c1e3154af7b5
Mohammed Nagdy <mohammednagdy@hotmail.com>
2023-03-12 23:48:42 +0200

    Added User Schema

--- Models/User/index.js
+++ Models/User/index.js
@@ -1,1 +1,13 @@
 // Create a user model schema
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.Model('User', UserSchema);
+
+module.exports = User;
