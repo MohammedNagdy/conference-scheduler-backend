a98eb8e3ba1def4bfa2db815aa94d79b955094af
Mohammed Nagdy <42354467+MohammedNagdy@users.noreply.github.com>
2023-03-25 15:49:57 +0000

    Created a user with encrypted password

--- Controllers/User/index.js
+++ Controllers/User/index.js
@@ -1,8 +1,8 @@
 // Express Validator
 const { body, validationResult } = require('express-validator');
+const crypto = require('crypto');
 
 const User = require('../../Models/User');
-const ifUserExists = require('../../utils/custom-validators');
 
 
 exports.login = (req, res, next) => {
@@ -12,11 +12,21 @@ exports.login = (req, res, next) => {
 
 exports.signup = (req, res, next) => {
     // Get user from data base
-    const name = req.body.name;
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+    const newUser = {
+      name : req.body.name,
+      username : req.body.username,
+      email : req.body.email,
+      password : crypto.pbkdf2Sync(req.body.password, process.env.salt,  
+        1000, 64, `sha512`).toString(`hex`),
+    }
    // TODO: Create a user with encrypted password
+   User.create(newUser)
+    .then( user => {
+      res.status(200);
+    }).catch( err => {
+      res.send(`Error ${err}`).status(500);
+    })
+
 }
 
 // Validation
